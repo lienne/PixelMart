ee3aaee4bc8c3ceecd09cb8133882f9924045eb0
Liana <lianasevero3@gmail.com>
2025-04-22 18:42:04 -0400

    Updating database credentials for production

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -3,13 +3,25 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: Number(process.env.PG_PORT),
-});
+let pool: Pool;
+
+if (process.env.DATABASE_URL) {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false, // Heroku Postgres requires SSL
+        },
+    });
+} else {
+    // Local development
+    pool = new Pool({
+        user: process.env.PG_USER,
+        host: process.env.PG_HOST,
+        database: process.env.PG_DATABASE,
+        password: process.env.PG_PASSWORD,
+        port: Number(process.env.PG_PORT),
+    });
+}
 
 export const testConnection = async () => {
     try {
